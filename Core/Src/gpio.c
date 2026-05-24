1d93158e28910d0c97ade0d9d27d11464dbd2a2e
SunXingzhi <q13258843359@qq.com>
2026-05-24 17:55:02 +0800

    新增对于GPIOB: PIN7~PIN9的配置->用于控制步进电机的步进模式(全步进--1/16步进), 控制方式是改变MS1~MS3的引脚电平.

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -50,7 +50,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(MOTOR_DIR_PIN_GPIO_Port, MOTOR_DIR_PIN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, MOTOR_DIR_PIN_Pin|MOTOR_MS1_PIN_Pin|MOTOR_MS2_PIN_Pin|MOTOR_MS3_PIN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : MOTOR_DIR_PIN_Pin */
   GPIO_InitStruct.Pin = MOTOR_DIR_PIN_Pin;
@@ -58,6 +58,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
   HAL_GPIO_Init(MOTOR_DIR_PIN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : MOTOR_MS1_PIN_Pin MOTOR_MS2_PIN_Pin MOTOR_MS3_PIN_Pin */
+  GPIO_InitStruct.Pin = MOTOR_MS1_PIN_Pin|MOTOR_MS2_PIN_Pin|MOTOR_MS3_PIN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
